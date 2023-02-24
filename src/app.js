a1019cb636c205b393ff82d6a80269ede4b8aecc
axcm2002 <a.x.c.m.2002@gmail.com>
2023-02-24 06:33:35 +0000

    fisrt commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  var pronombres = ["the", "our"];
+  var adj = ["great", "amazing", "big", "fantastic", "fabulous"];
+  var noun = ["horse", "turttle", "cat", "dog"];
+
+  function junte() {
+    for (let i = 0; i < adj.length; i++) {
+      for (let b = 0; b < noun.length; b++) {
+        for (let a = 0; a < pronombres.length; a++) {
+          console.log("www." + pronombres[i] + adj[b] + noun[a] + ".com");
+        }
+      }
+    }
+  }
+  junte();
+
   console.log("Hello Rigo from the console!");
 };
